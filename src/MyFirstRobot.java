a064d684cd122354b23676fce66ca1cda0175322
Kuttesch <moritzkuttesch@outlook.de>
2024-12-20 00:33:06 +0100

    added java file

---
+++ src/MyFirstRobot.java
@@ -0,0 +1,23 @@
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+package robocode;
+
+
+public class MyFirstRobot extends Robot {
+
+    @Override
+    public void run() {
+        while (true) {
+            ahead(100); // Move ahead 100 units
+            turnGunRight(360); // Scan for other robots
+            back(100); // Move back 100 units
+            turnGunRight(360); // Scan for other robots
+        }
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+        fire(1); // Fire a bullet with power 1
+    }
+}
